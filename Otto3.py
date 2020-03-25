6fe3f26e90223b7b4f1b89cb68cde14ea6278960
MikeyBeez <mbonsign@gmail.com>
2020-03-25 05:00:06 -0400

    purdier and purdier comments!

--- Otto3.py
+++ Otto3.py
@@ -16,18 +16,21 @@ import pyautogui
 
 ###############################################################################################
 
-######## THIS IS AIML STUFF
+######## THIS IS AIML SETUP STUFF
 
+# make a variable for the file name
 BRAIN_FILE="brain.dump"
 
+# This is creating a kernel object from the imported aiml module
 brainkernel = aiml.Kernel()
 
-# To increase the startup speed of the bot it is
-# possible to save the parsed aiml files as a
-# dump. This code checks if a dump exists and
-# otherwise loads the aiml from the xml files
-# and saves the brain dump.
+# To increase the startup speed of the bot, it is
+# possible to save the parsed aiml files as a dump.
+# This code checks if a dump exists, and
+# otherwise loads the aiml from the xml files.
+# Then it saves the brain dump as brain.dump.
 
+# the kernel object we just made is empty.  We need to load it.
 if os.path.exists(BRAIN_FILE):
     print("Loading from brain file: " + BRAIN_FILE)
     brainkernel.loadBrain(BRAIN_FILE)
@@ -37,19 +40,29 @@ else:
     print("Saving brain file: " + BRAIN_FILE)
     brainkernel.saveBrain(BRAIN_FILE)
 
-######## END AIML STUFF
+######## END AIML SETUP STUFF
 
 ###############################################################################################
 
 ######## TTS TEXT TO SPEECH FUNCTION 
 
+# This gets used all over to speak text aloud.
+# It also prints to the console for people with bad memories.
+
 def talkToMe(mytext):
     # "speaks audio passed as argument"
 
     print(mytext)
+    # can handle multiline text.
     for line in mytext.splitlines():
+        # uses the google text to speech module to synthesize text
         text_to_speech = gTTS(text=mytext, lang='en')
+        # saves syntesized speech to audio.mp3
+        # this file gets written, played. and overwritten
+        # over and over again.
         text_to_speech.save('audio.mp3')
+        # the sox modules wrapper is mpg123.
+        # This is called by the operating system imported os module.
         os.system('mpg123 -q audio.mp3')
 
 ######## END TTS TEXT TO SPEECH FUNCTION 
@@ -61,22 +74,30 @@ def talkToMe(mytext):
 def myCommand():
     # "listens for commands"
 
+    # We imported this up above "import speech_recognition as sr"
+    # We create a recognizer object and assign it to the variable r.
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
         print('Ready\n ')
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source, duration=1)
+        # Here we create the variable audio and fill it with captured audio.
         audio = r.listen(source)
 
     try:
+        # Here we create the variable command and fill it with text converted from audio.
         command = r.recognize_google(audio).lower()
         print('You said: ' + command + '\n')
 
-    #loop back to continue to listen for commands if unrecognizable speech is received
+    # This except block is catching errors if the try block fails.
+    # loop back to continue to listen for commands if unrecognizable speech is received
     except sr.UnknownValueError:
         print('Your last command couldn\'t be heard')
         command = myCommand();
+
+    # This tiny line is important. It is returning the command variable with the
+    # new value we just set.  It will be used later.
 
     return command
 
