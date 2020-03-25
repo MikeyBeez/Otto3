62ec81e056b3383544b14bfe6dabdd1f537199c6
MikeyBeez <mbonsign@gmail.com>
2020-03-25 00:20:44 -0400

    easy shell script with proper environment

---
+++ otto.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# place this script in ~/bin
+# you must use the correct path to the python in whatever virtual 
+# environment you built this project in, and point the right path 
+# to otto3.py
+# chmod +x ~/bin/otto.sh
+# put 
+# alias otto="~/bin/otto.sh"
+/home/bard/miniconda3/envs/Otto/bin/python /home/bard/Code/Otto3/Otto3.py
+#  then you can type otto & to start otto in the background
