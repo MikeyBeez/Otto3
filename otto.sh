722945ce08fec93ad439ed7b98ea7e05a9ee8e54
MikeyBeez <mbonsign@gmail.com>
2020-03-27 03:49:46 -0400

    redirected ALSA errors to /dev/null

--- otto.sh
+++ otto.sh
@@ -7,5 +7,5 @@
 # chmod +x ~/bin/otto.sh
 # put 
 # alias otto="~/bin/otto.sh"
-/home/bard/miniconda3/envs/Otto/bin/python /home/bard/Code/Otto3/Otto3.py
+/home/bard/miniconda3/envs/Otto/bin/python /home/bard/Code/Otto3/Otto3.py 2>/dev/null
 #  then you can type otto & to start otto in the background
