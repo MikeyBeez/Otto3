be09b74984fce85c1f5f38a6e4fc6b7423f238a4
MikeyBeez <mbonsign@gmail.com>
2020-03-31 00:09:27 -0400

    formated mymusiclist added backslash to spaces

--- chatbot.py
+++ chatbot.py
@@ -1,28 +1,28 @@
 #!/home/bard/miniconda3/envs/Otto/bin/python3
-import os
-import aiml
+import\ os
+import\ aiml
 
 BRAIN_FILE="brain.dump"
 
-k = aiml.Kernel()
+k\ =\ aiml.Kernel()
 
-# To increase the startup speed of the bot it is
-# possible to save the parsed aiml files as a
-# dump. This code checks if a dump exists and
-# otherwise loads the aiml from the xml files
-# and saves the brain dump.
-if os.path.exists(BRAIN_FILE):
-    print("Loading from brain file: " + BRAIN_FILE)
-    k.loadBrain(BRAIN_FILE)
+#\ To\ increase\ the\ startup\ speed\ of\ the\ bot\ it\ is
+#\ possible\ to\ save\ the\ parsed\ aiml\ files\ as\ a
+#\ dump.\ This\ code\ checks\ if\ a\ dump\ exists\ and
+#\ otherwise\ loads\ the\ aiml\ from\ the\ xml\ files
+#\ and\ saves\ the\ brain\ dump.
+if\ os.path.exists(BRAIN_FILE):
+\ \ \ \ print("Loading\ from\ brain\ file:\ "\ +\ BRAIN_FILE)
+\ \ \ \ k.loadBrain(BRAIN_FILE)
 else:
-    print("Parsing aiml files")
-    k.bootstrap(learnFiles="std-startup.aiml", commands="load aiml b")
-    print("Saving brain file: " + BRAIN_FILE)
-    k.saveBrain(BRAIN_FILE)
+\ \ \ \ print("Parsing\ aiml\ files")
+\ \ \ \ k.bootstrap(learnFiles="std-startup.aiml",\ commands="load\ aiml\ b")
+\ \ \ \ print("Saving\ brain\ file:\ "\ +\ BRAIN_FILE)
+\ \ \ \ k.saveBrain(BRAIN_FILE)
 
-# Endless loop which passes the input to the bot and prints
-# its response
-while True:
-    input_text = input("> ")
-    response = k.respond(input_text)
-    print(response)
+#\ Endless\ loop\ which\ passes\ the\ input\ to\ the\ bot\ and\ prints
+#\ its\ response
+while\ True:
+\ \ \ \ input_text\ =\ input(">\ ")
+\ \ \ \ response\ =\ k.respond(input_text)
+\ \ \ \ print(response)
